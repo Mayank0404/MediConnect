eaa397daddde0d2b2e844cf23fd47debd72eb53f
Mayank0404 <mayanksharma040404@gmail.com>
2026-07-22 01:14:12 +0530

    Initial commit

---
+++ src/test/java/com/mayank/mediconnect/mediconnect/MediconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.mediconnect.mediconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
